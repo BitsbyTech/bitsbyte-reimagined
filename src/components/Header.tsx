578f0ba57fa3d5571a0ef846d9b72e894354caf5
b1t53c <49392080+Wambugu-dennis@users.noreply.github.com>
2026-01-26 14:26:38 +0300

    header edits

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,7 @@ const Header = () => {
             {/* Logo */}
             <a href="/" className="flex items-center space-x-2">
               <img src={logo} alt="BitsbyTech Logo" className="h-[65px] w-[40px] object-contain" />
-              <span className="text-xl font-bold">BitsbyTech</span>
+              <span className="text-xl font-bold">BitsbyTech labs</span>
             </a>
 
             {/* Desktop Navigation */}
